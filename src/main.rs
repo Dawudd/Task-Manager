570a28df9dd89b9889d011a81287a35dc32a964d
Dawid Szłapa <dawidszlapaz@gmail.com>
2025-06-13 15:49:33 +0200

    Clear console

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,19 @@ pub mod task;
 mod csv_handler;
 mod task_manager;
 
+fn clear_console() {
+    if cfg!(target_os = "windows") {
+        std::process::Command::new("cmd")
+            .args(&["/C", "cls"])
+            .status()
+            .expect("Failed to clear console");
+    } else {
+        std::process::Command::new("clear")
+            .status()
+            .expect("Failed to clear console");
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
